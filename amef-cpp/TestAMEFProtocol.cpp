34c1c46a0f8fb4232f23b33ed8185e8862537562
sumeet.chhetri <sumeet.chhetri@67507bcd-ef77-e452-7ae9-9c96e31ba17d>
2011-09-03 10:04:25 +0000

    update code and restructure it

---
+++ amef-cpp/TestAMEFProtocol.cpp
@@ -0,0 +1,38 @@
+/*
+	Copyright 2010, Sumeet Chhetri 
+  
+    Licensed under the Apache License, Version 2.0 (the "License"); 
+    you may not use this file except in compliance with the License. 
+    You may obtain a copy of the License at 
+  
+        http://www.apache.org/licenses/LICENSE-2.0 
+  
+    Unless required by applicable law or agreed to in writing, software 
+    distributed under the License is distributed on an "AS IS" BASIS, 
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+    See the License for the specific language governing permissions and 
+    limitations under the License.  
+*/
+#include "AMEFDecoder.h"
+#include "AMEFEncoder.h"
+#include <iostream>
+int main()
+{
+	AMEFObject *object = new AMEFObject();
+	string h = "This is the Automated Message Exchange Format Object property!!";
+	object->addPacket("This is the Automated Message Exchange Format Object property!!","adasd");	
+	object->addPacket(21213);
+	object->addPacket(true);
+	AMEFObject *object2 = new AMEFObject();
+	string j = "This is the property of a nested Automated Message Exchange Format Object";
+	object2->addPacket(j);
+	object2->addPacket(134123);
+	object2->addPacket(false);
+	object2->addPacket('d');
+	object->addPacket(object2);
+	string str = new AMEFEncoder()->encode(object,false);
+	cout << str << flush;
+	AMEFObject *object1 = new AMEFDecoder()->decode(str,true,false);
+	cout << object1->displayObject("") << flush;
+	return 1;
+}
